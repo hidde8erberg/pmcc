390a55ec7395361b88df0abd4c5cefd93ee78b73
Hidde <hidde8erberg@gmail.com>
2018-03-31 09:30:12 +0200

    y coords, plot, comments

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-x = [100.25, 100.31, 100.34, 100.66, 100.76, 100.88, 101.02, 101.09, 101.15, 101.5, 101.45, 102.86, 103.18, 103.36, 103.29, 104.06, 104.44, 104.45, 104.53, 104.94, 104.93, 105.31, 105.64, 105.69]
-y = [3030.25, 100.31, 100.34, 100.66, 100.76, 100.88, 101.02, 101.09, 101.15, 101.5, 101.45, 102.86, 103.18, 103.36, 103.29, 104.06, 104.44, 104.45, 104.53, 104.94, 104.93, 105.31, 105.64, 105.69]
+# de x en y waarden voor de coordinaten
+x = [100.25, 100.31, 100.34, 100.66, 100.76, 100.88, 101.02, 101.09, 101.15, 101.5, 101.45, 102.86, 103.18, 103.29, 103.36, 103.29, 104.06, 104.44, 104.45, 104.53, 104.94, 104.93, 105.31, 105.64, 105.69]
+y = [608, 617, 591, 565, 603, 612, 599, 587, 557, 528, 517, 484, 514, 510, 495, 458, 509, 504, 489, 471, 456, 425, 413, 388, 424]
 
-if len(x) != len(y):
-    print("Arrays not equal size!")
-    quit()
+if len(x) != len(y):  # check dat er voor elk coordinaat een x en een y waarde is
+    print("Arrays are not equally sized!", "[", len(x), len(y), "]")
+    quit(1)
 
-def sd(arr=[]):
+
+def sd(arr=[]):  # berekening van de standaard afwijking
     rsum = 0
     for i in arr:
         rsum += i
@@ -20,20 +23,25 @@ def sd(arr=[]):
     return np.sqrt(arrdif / len(arr)), arrav
 
 
-def z(x, arr=[]):
+def z(x, arr=[]):  # berekend Zx of Zy
     stddvn, arrav = sd(arr)
     return (x - arrav) / stddvn
 
 
-def zz(arr1=[], arr2=[]):
+def zz(arr1=[], arr2=[]):  # som van Zx*Zy
     zsum = 0
     for i in range(len(arr1)):
         zsum += z(arr1[i], arr1) * z(arr2[i], arr2)
     return zsum
 
 
-def pmcc(arr1=[], arr2=[]):
+def pmcc(arr1=[], arr2=[]):  # deelt de som van Zx*Zy door n
     return zz(arr1, arr2) / len(arr1)
 
 
-print(pmcc(x, y))
+print(pmcc(x, y))  # print de correlatie coefficient naar het scherm
+
+# een plot van de coordinaten
+plt.plot(x, y, "ro")
+plt.axis([100, 106, 350, 650])
+plt.show()
