e337a969960b1cb1c7a20d116e0aac69f35b6b2c
Hidde <hidde8erberg@gmail.com>
2018-04-01 10:56:45 +0200

    final touches

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 # de x en y waarden voor de coordinaten
 x = [100.25, 100.31, 100.34, 100.66, 100.76, 100.88, 101.02, 101.09, 101.15, 101.5, 101.45, 102.86, 103.18, 103.29, 103.36, 103.29, 104.06, 104.44, 104.45, 104.53, 104.94, 104.93, 105.31, 105.64, 105.69]
 y = [608, 617, 591, 565, 603, 612, 599, 587, 557, 528, 517, 484, 514, 510, 495, 458, 509, 504, 489, 471, 456, 425, 413, 388, 424]
+yShuffle = [514, 591, 509, 489, 617, 599, 603, 608, 565, 456, 510, 557, 504, 424, 458, 484, 528, 388, 495, 517, 471, 425, 413, 587, 612]
+xShuffle = [101.09, 102.86, 104.94, 100.25, 103.29, 103.29, 101.5, 100.31, 104.93, 100.88, 101.02, 103.18, 105.69, 103.36, 101.45, 105.64, 100.76, 104.45, 100.34, 104.06, 100.66, 104.53, 104.44, 105.31, 101.15]
+
 
 if len(x) != len(y):  # check dat er voor elk coordinaat een x en een y waarde is
     print("Arrays are not equally sized!", "[", len(x), len(y), "]")
@@ -39,6 +42,10 @@ def pmcc(arr1=[], arr2=[]):  # deelt de som van Zx*Zy door n
     return zz(arr1, arr2) / len(arr1)
 
 
+def line(x):
+    return rc * x + yinterc
+
+
 r = pmcc(x, y)
 print(r)  # print de correlatie coefficient naar het scherm
 
@@ -46,10 +53,6 @@ sdX, xmean = sd(x)
 sdY, ymean = sd(y)
 rc = r * (sdY / sdX)
 yinterc = ymean - (rc * xmean)
-
-
-def line(x):
-    return rc * x + yinterc
 
 
 # een plot van de coordinaten
